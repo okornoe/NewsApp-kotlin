f65454ca8e6705cc929b890656d09eda2a4d8dd2
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-05-23 18:39:11 +0000

    add home news fragment and its activity

---
+++ app/src/main/java/com/t/newsapp_kotlin/NewsHomeFragment.kt
@@ -0,0 +1,24 @@
+package com.t.newsapp_kotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.t.newsapp_kotlin.databinding.FragmentNewsHomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class NewsHomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentNewsHomeBinding>(inflater, R.layout.fragment_news_home, container, false)
+        return binding.root
+    }
+
+}
