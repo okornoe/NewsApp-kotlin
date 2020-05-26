25050a6bd619092664c43bf2de1302755d7802a8
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-05-26 15:16:32 +0000

    view holder class for news Item

---
+++ app/src/main/java/com/t/newsapp_kotlin/NewsViewHolder.kt
@@ -0,0 +1,8 @@
+package com.t.newsapp_kotlin
+
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsViewHolder (val textView: TextView) : RecyclerView.ViewHolder(textView) {
+}
