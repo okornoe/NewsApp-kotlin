0c5049aa21b8355dcbe85e3781ec489dd7b342c8
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-05-27 18:20:48 +0000

    add moshi library to retrieve json data

--- app/src/main/java/com/t/newsapp_kotlin/NewsApiService.kt
+++ app/src/main/java/com/t/newsapp_kotlin/NewsApiService.kt
@@ -1,22 +1,29 @@
 package com.t.newsapp_kotlin
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://newsapi.org/v2/"
 private const val API_KEY = "f7566e894ffd42f389814d672b2714cf"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
 interface NewsApiService {
     @GET("top-headlines?country=us&apiKey=${API_KEY}")
     fun getProperties():
-            Call<String>
+            Call<NewsData>
 }
 
 object NewsApi {
