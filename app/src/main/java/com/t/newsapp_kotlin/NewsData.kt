268f1fdfe2507fa0555636a9127db539fa9b8f62
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-05-30 12:12:12 +0000

    add Article data class and Source data class

--- app/src/main/java/com/t/newsapp_kotlin/NewsData.kt
+++ app/src/main/java/com/t/newsapp_kotlin/NewsData.kt
@@ -1,6 +1,24 @@
 package com.t.newsapp_kotlin
 
-data class NewsData (
-    val author: String, val title: String,
-    val url: String, val publishedAt: String
+data class NewsData(
+    val articles: List<Article>?
+    //val status: String?,
+    //val totalResults: Int?
 )
+
+data class Article(
+    val author: Any?,
+    //val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    //val source: Source?,
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?
+)
+
+data class Source(
+    val id: Any?,
+    val name: String?
+)
+
