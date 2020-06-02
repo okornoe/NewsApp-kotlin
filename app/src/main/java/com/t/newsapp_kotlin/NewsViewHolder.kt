4ba150218cfce2c005febdca828537b374ada0a0
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-06-02 19:16:31 +0000

    implement viewHolder

--- app/src/main/java/com/t/newsapp_kotlin/NewsViewHolder.kt
+++ app/src/main/java/com/t/newsapp_kotlin/NewsViewHolder.kt
@@ -1,9 +1,15 @@
 package com.t.newsapp_kotlin
 
-import android.widget.LinearLayout
+import android.view.TextureView
+import android.view.View
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
-/*
-class NewsViewHolder (val textView: TextView) : RecyclerView.ViewHolder(textView) {
-}*/
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var author: TextView? = itemView.findViewById(R.id.tv_author_name)
+    val title: TextView? = itemView.findViewById(R.id.tv_new_title)
+    val description: TextView? = itemView.findViewById(R.id.tv_news_description)
+    val publishedAt: TextView? = itemView.findViewById(R.id.tv_date_publication)
+}
