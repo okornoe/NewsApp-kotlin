775731548efc02107d38d03f09aa3f2b76d6017b
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-06-02 19:15:06 +0000

    onBindView implementation

--- app/src/main/java/com/t/newsapp_kotlin/NewsAdapter.kt
+++ app/src/main/java/com/t/newsapp_kotlin/NewsAdapter.kt
@@ -1,24 +1,33 @@
 package com.t.newsapp_kotlin
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
-//class NewsAdapter : RecyclerView.Adapter<NewsViewHolder>() {
-/*    val listNames = listOf<String>("Kelvin", "Okornoe", "Joseph", "Anthony")
+class NewsAdapter : RecyclerView.Adapter<NewsViewHolder>() {
+    private val newsData = listOf<Article>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val inflaterLayout = LayoutInflater.from(parent.context)
-        val view = inflaterLayout.inflate(R.layout.news_item,parent,false) as TextView
+
+        val view = inflaterLayout.inflate(R.layout.news_item,parent,false) as View
         return NewsViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        return listNames.size;
+        return newsData.size;
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        val item = listNames[position]
-        holder.textView.text = item
-    }*/
-//}
+        val item = newsData[position]
+        //holder.
+        holder.author?.text   = item.author
+        holder.title?.text  = item.title
+        holder.description?.text  = item.description
+        holder.publishedAt?.text = item.publishedAt
+    }
+}
+
