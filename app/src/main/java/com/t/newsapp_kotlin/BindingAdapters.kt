2cc03adfa70480a7531ccaf740b22c9bc2b5d304
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-06-05 16:05:01 +0000

    Binding adapter to process images

---
+++ app/src/main/java/com/t/newsapp_kotlin/BindingAdapters.kt
@@ -0,0 +1,17 @@
+package com.t.newsapp_kotlin
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("urlToImage")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .into(imgView)
+    }
+}
