58182102e54f0fb6e5fb5672351da3547cd4ca55
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-06-06 14:29:15 +0000

    binding view model

--- app/src/main/java/com/t/newsapp_kotlin/NewsHomeFragment.kt
+++ app/src/main/java/com/t/newsapp_kotlin/NewsHomeFragment.kt
@@ -27,14 +27,16 @@ class NewsHomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentNewsHomeBinding>(inflater,
-            R.layout.fragment_news_home, container, false)
+        //val binding = DataBindingUtil.inflate<FragmentNewsHomeBinding>(inflater,
+        //    R.layout.fragment_news_home, container, false)
+        val binding = FragmentNewsHomeBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.setLifecycleOwner (this)
 
         // Giving the binding access to the NewsViewModel
-        binding.viewModel = NewsAdapter()
+        binding.viewModel = viewModel
+        //binding.newsItem.adapter = NewsAdapter()
 
 
         return binding.root
