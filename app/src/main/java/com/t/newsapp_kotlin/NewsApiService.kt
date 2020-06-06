9f4cd16b7b25f2d59048a889ff324997b4edc259
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-06-06 14:28:33 +0000

    remove unused comments

--- app/src/main/java/com/t/newsapp_kotlin/NewsApiService.kt
+++ app/src/main/java/com/t/newsapp_kotlin/NewsApiService.kt
@@ -6,13 +6,10 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.ToJson
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
-import okhttp3.ResponseBody
-import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Response
+
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
+//import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://newsapi.org/v2/"
