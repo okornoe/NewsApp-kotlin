87c728d386afd270b78158522edc89fa831f934b
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-06-06 14:30:10 +0000

    new viewholder implementation

--- app/src/main/java/com/t/newsapp_kotlin/NewsViewHolder.kt
+++ app/src/main/java/com/t/newsapp_kotlin/NewsViewHolder.kt
@@ -1,15 +1,13 @@
 package com.t.newsapp_kotlin
 
-import android.view.TextureView
-import android.view.View
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.t.newsapp_kotlin.databinding.NewsItemBinding
+
+class NewsViewHolder(private var binding: NewsItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(articles: List<Article>?) {
 
 
-class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    var author: TextView? = itemView.findViewById(R.id.tv_author_name)
-    val title: TextView? = itemView.findViewById(R.id.tv_new_title)
-    val description: TextView? = itemView.findViewById(R.id.tv_news_description)
-    val publishedAt: TextView? = itemView.findViewById(R.id.tv_date_publication)
+        binding.executePendingBindings()
+    }
 }
